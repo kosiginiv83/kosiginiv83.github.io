b0168bca72d03d53266ee95171e09b91d6456d6c
kosiginiv83 <kosiginiv83@ADMIN-LAPTOP>
2018-01-24 10:30:46 +0800

    no message

--- 1.js
+++ 1.js
@@ -1,17 +1,19 @@
-$(function(f){
-  var element = f('#header-block1');
-  f(window).scroll(function(){
-      element['fade'+ (f(this).scrollTop() < 400 ? 'In': 'Out')](100);
+<script type="text/javascript" >
+  $(function(f){
+    var element = f('#header-block1');
+    f(window).scroll(function(){
+        element['fade'+ (f(this).scrollTop() < 400 ? 'In': 'Out')](100);
+    });
   });
-});
 
-function windowSize(){
-  if ($(window).width() >= '1008'){
-    $('#eye').show(10);
-    $('#abstract').show(10);
-  } else {
-    $('#eye').hide(10);
-    $('#abstract').hide(10);
+  function windowSize(){
+    if ($(window).width() >= '1008'){
+      $('#eye').show(10);
+      $('#abstract').show(10);
+    } else {
+      $('#eye').hide(10);
+      $('#abstract').hide(10);
+    }
   }
-}
-$(window).on('load resize',windowSize);
+  $(window).on('load resize',windowSize);
+</script>
